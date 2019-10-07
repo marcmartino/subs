29781b23e008220cde02fa71c53c0fb46b942989
CORP\marc.martino <marc.martino@vegas.com>
2019-10-07 11:34:49 -0700

    cleaned up sacc sub method to better offer different stepping options

--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,9 @@ const flattenList = <T>(subsList: T[], someSubs: T[]) => [
   ...someSubs
 ];
 
-/**
- * Finds a saccharide substitution
- *
- * @param {Triple<number, number, number>} targets Target qty, pod/sweetness, freezing point depression/pac.
- * @param {subOption[][]} substitution_options list of triples denoting the options suited for substitutions
- * @return {string[]} either a list representing the found substitution, or a single that says no substitutions found
- * @customfunction
- */
-function SACCHARIDESUB(
+function saccSub(step: number, 
   [[targetQty, targetPOD, targetPAC]]: [Triple<number, number, number>],
-  ...substitutionOptions: subOption[][]
+  substitutionOptions: subOption[][]
 ): string[] {
   const subs: subOption[] = substitutionOptions.reduce(flattenList, []);
   const calculatedOptions = calculateSubstitutionOptions(
@@ -37,51 +29,48 @@ function SACCHARIDESUB(
     ? calculatedOptions.reduce(formatReturnList(subs, targetQty), [])
     : ["No Substitutions Found"];
 }
-function SACCHARIDESUBQUARTER(
-  [[targetQty, targetPOD, targetPAC]]: [Triple<number, number, number>],
+
+/**
+ * Finds a saccharide substitution with 1/2 accuracy
+ *
+ * @param {Triple<number, number, number>} targets Target qty, pod/sweetness, freezing point depression/pac.
+ * @param {subOption[][]} substitution_options list of triples denoting the options suited for substitutions
+ * @return {string[]} either a list representing the found substitution, or a single that says no substitutions found
+ * @customfunction
+ */
+function SACCHARIDESUBHALVES(
+  targets: [Triple<number, number, number>],
   ...substitutionOptions: subOption[][]
 ): string[] {
-  const subs: subOption[] = substitutionOptions.reduce(flattenList, []);
-  const calculatedOptions = calculateSubstitutionOptions(
-    targetQty,
-    [targetPOD, subs.map(snd)],
-    [targetPAC, subs.map(thrd)],
-    0.25
-  );
-  return calculatedOptions
-    ? calculatedOptions.reduce(formatReturnList(subs, targetQty), [])
-    : ["No Substitutions Found"];
-}
-function SACCHARIDESUBTENTHS(
-  [[targetQty, targetPOD, targetPAC]]: [Triple<number, number, number>],
-  ...substitutionOptions: subOption[][]
-): string[] {
-  const subs: subOption[] = substitutionOptions.reduce(flattenList, []);
-  const calculatedOptions = calculateSubstitutionOptions(
-    targetQty,
-    [targetPOD, subs.map(snd)],
-    [targetPAC, subs.map(thrd)],
-    0.1
-  );
-  return calculatedOptions
-    ? calculatedOptions.reduce(formatReturnList(subs, targetQty), [])
-    : ["No Substitutions Found"];
-}
-function SACCHARIDESUBHUNDRETHS(
-  [[targetQty, targetPOD, targetPAC]]: [Triple<number, number, number>],
-  ...substitutionOptions: subOption[][]
-): string[] {
-  const subs: subOption[] = substitutionOptions.reduce(flattenList, []);
-  const calculatedOptions = calculateSubstitutionOptions(
-    targetQty,
-    [targetPOD, subs.map(snd)],
-    [targetPAC, subs.map(thrd)],
-    0.01
-  );
-  return calculatedOptions
-    ? calculatedOptions.reduce(formatReturnList(subs, targetQty), [])
-    : ["No Substitutions Found"];
+  return saccSub(.5, targets, substitutionOptions)
 }
 
-//@ts-ignore
-window.SACCHARIDESUB = SACCHARIDESUB;
+/**
+ * Finds a saccharide substitution with tenths accuracy
+ *
+ * @param {Triple<number, number, number>} targets Target qty, pod/sweetness, freezing point depression/pac.
+ * @param {subOption[][]} substitution_options list of triples denoting the options suited for substitutions
+ * @return {string[]} either a list representing the found substitution, or a single that says no substitutions found
+ * @customfunction
+ */
+function SACCHARIDESUBTENTHS(
+  targets: [Triple<number, number, number>],
+  ...substitutionOptions: subOption[][]
+): string[] {
+  return saccSub(.1, targets, substitutionOptions)
+}
+
+/**
+ * Finds a saccharide substitution with hundredths accuracy
+ *
+ * @param {Triple<number, number, number>} targets Target qty, pod/sweetness, freezing point depression/pac.
+ * @param {subOption[][]} substitution_options list of triples denoting the options suited for substitutions
+ * @return {string[]} either a list representing the found substitution, or a single that says no substitutions found
+ * @customfunction
+ */
+function SACCHARIDESUBHUNDRETHS(
+  targets: [Triple<number, number, number>],
+  ...substitutionOptions: subOption[][]
+): string[] {
+  return saccSub(.01, targets, substitutionOptions)
+}
