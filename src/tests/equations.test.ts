6432a259d4084a6bcf7be3e43aacc860e8fb5238
CORP\marc.martino <marc.martino@vegas.com>
2019-10-07 11:38:12 -0700

    updated eq tests

--- src/tests/equations.test.ts
+++ src/tests/equations.test.ts
@@ -11,14 +11,6 @@ describe("solving for a possible substitution", function() {
     assert.isFalse(isInvalidSolution(0), "minimum should be valid");
     
     assert.isTrue(isInvalidSolution(-1), "less than zero should be true");
-    // assert.isTrue(
-    //   isInvalidSolution(100)(2 / 3),
-    //   "repeating decimal should be true"
-    // );
-    // assert.isFalse(
-    //   isInvalidSolution(100)(3 / 2),
-    //   "non-repeating decimal should be false"
-    // );
   });
 
   it("should find appropriate variable maximums", function() {
@@ -49,7 +41,7 @@ describe("solving for a possible substitution", function() {
   it("should brute force a the first value and calculate the others for substitutions", function() {
     assert.deepEqual(
       solve(20, [14, 17], [[1, 2, 3], [0, 7, 1]]),
-      [1, 2, 3],
+      [9, 2.4210526315789473, 0.05263157894736836],
       "simple brute force option substitution"
     );
     assert.deepEqual(
@@ -59,25 +51,10 @@ describe("solving for a possible substitution", function() {
     );
   });
   it("should brute force a the first two values and calculate the others for substitutions", function() {
-    // assert.deepEqual(
-    //   solve(5, [12, 20], [[1, 2, 3, 4], [3, 1, 3, 1]]),
-    //   [5, 2, 1, 0],
-    //   "multiple brute forced values"
-    // );
-    // assert.deepEqual(
-    //   solve(154, [120, 190], [[130,130,48,100],[146,167,80,100]]),
-    //   [1,2,3,4],
-    //   "should not deliver negitive values"
-    // )
-    // assert.equal(
-    //   solve(105, [120, 190], [[130, 42, 100], [167, 72, 100]]),
-    //   false,
-    //   "should solve with positive volumes"
-    // );
     assert.deepEqual(
-      solve(300, [77.5, 90], [[130, 70, 170], [146, 190, 190]]),
-      [1, 2, 3],
-      "honey, dextrose, fructose example"
+      solve(5, [12, 20], [[1, 2, 3, 4], [3, 1, 3, 1]]),
+      [5, 2, 1, 0],
+      "multiple brute forced values"
     );
   });
 });
